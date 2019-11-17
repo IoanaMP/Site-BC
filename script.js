f4ab13cb30bc3aa897ba8eca59f4fded918a8bb3
robertzanceanu <43026706+robertzanceanu@users.noreply.github.com>
2019-11-17 18:21:23 +0200

    Responsive and other changes

---
+++ script.js
@@ -0,0 +1,34 @@
+var countDownDate = new Date("Dec 16, 2019 16:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+  document.getElementById("days1").innerHTML = Math.floor(days/10)
+  document.getElementById("days2").innerHTML = days%10
+
+  document.getElementById("hours1").innerHTML = Math.floor(hours/10)
+  document.getElementById("hours2").innerHTML = hours%10
+
+  document.getElementById("minutes1").innerHTML = Math.floor(minutes/10)
+  document.getElementById("minutes2").innerHTML = minutes%10
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
