12cb5e593b4071765e585260d2f2f963062743af
Flavius <aflaviusandrei@gmail.com>
2019-12-03 10:32:53 +0200

    Fixed Palas countdown (it's now counting up to 14th December, 11 AM)

--- script.js
+++ script.js
@@ -33,6 +33,41 @@ var x = setInterval(function() {
   }
 }, 1000);
 
+var pcountDownDate = new Date("Dec 14, 2019 11:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = pcountDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+  document.getElementById("p-days1").innerHTML = Math.floor(days/10)
+  document.getElementById("p-days2").innerHTML = days%10
+
+  document.getElementById("p-hours1").innerHTML = Math.floor(hours/10)
+  document.getElementById("p-hours2").innerHTML = hours%10
+
+  document.getElementById("p-minutes1").innerHTML = Math.floor(minutes/10)
+  document.getElementById("p-minutes2").innerHTML = minutes%10
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("p-demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
 const openMenu = document.getElementById("open-menu"),
       closeMenu = document.getElementById("close-button"),
       menu = document.getElementsByClassName("menu-mobile")[0],
